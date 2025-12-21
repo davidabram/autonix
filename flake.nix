4e1ad2f5db7bbf81f4612c85b8c94e36dd222e28
David Abram <david@crocoder.dev>
2025-12-21 14:46:08 +0100

    feat: nix + rust

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "autonix"
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            cargo
+            rustc
+            rust-analyzer
+            clippy
+            rustfmt
+
+            bacon
+            cargo-watch
+            cargo-edit
+          ];
+
+          shellHook = ''
+            echo "nix-detect development environment"
+            echo "Rust version: $(rustc --version)"
+          '';
+
+          RUST_BACKTRACE = "1";
+        };
+      }
+    );
+}
+
