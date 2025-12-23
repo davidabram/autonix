a259e0ebb20c527d44d2b62150613d0f61db7273
David Abram <david@crocoder.dev>
2025-12-23 14:42:27 +0100

    tests: version detection tests
    add unit tests to run in nix flake check
    format everything!

--- src/detection/mod.rs
+++ src/detection/mod.rs
@@ -25,26 +25,30 @@ impl DetectionEngine {
     }
 
     pub fn detect(&self, path: &Path) -> ProjectMetadata {
-        let languages: Vec<LanguageDetection> = DirectoryIterator(VecDeque::from([path.to_path_buf()]))
-            .filter_map(|path| LanguageDetectionSignal::try_from(path).ok())
-            .fold(
-                HashMap::<Language, Vec<LanguageDetectionSignal>>::new(),
-                |mut acc, signal| {
-                    let lang = (&signal).into();
-                    acc.entry(lang).or_default().push(signal);
-                    acc
-                },
-            )
-            .into_iter()
-            .map(|(language, sources)| LanguageDetection::new(language, sources))
-            .collect();
+        let languages: Vec<LanguageDetection> =
+            DirectoryIterator(VecDeque::from([path.to_path_buf()]))
+                .filter_map(|path| LanguageDetectionSignal::try_from(path).ok())
+                .fold(
+                    HashMap::<Language, Vec<LanguageDetectionSignal>>::new(),
+                    |mut acc, signal| {
+                        let lang = (&signal).into();
+                        acc.entry(lang).or_default().push(signal);
+                        acc
+                    },
+                )
+                .into_iter()
+                .map(|(language, sources)| LanguageDetection::new(language, sources))
+                .collect();
 
         let versions = languages
             .iter()
             .filter_map(VersionDetection::from_language_detection)
             .collect();
 
-        ProjectMetadata { languages, versions }
+        ProjectMetadata {
+            languages,
+            versions,
+        }
     }
 }
 
@@ -54,7 +58,9 @@ impl Iterator for DirectoryIterator {
     type Item = PathBuf;
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop_front().inspect(|p| {
-            if p.is_dir() && let Ok(entries) = p.read_dir() {
+            if p.is_dir()
+                && let Ok(entries) = p.read_dir()
+            {
                 entries
                     .filter_map(|entry| entry.ok())
                     .for_each(|entry| self.0.push_back(entry.path()));
